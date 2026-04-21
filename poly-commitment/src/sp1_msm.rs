403abd506646c52f38a0bbfe28700c450dfdfe50
yout <yout@msn.com>
2026-04-21 22:30:22 +0200

    use syscall_uint256_mulmod

--- poly-commitment/src/sp1_msm.rs
+++ poly-commitment/src/sp1_msm.rs
@@ -84,12 +84,9 @@ impl Fp {
             let mut result = [0u64; 4];
             #[allow(unsafe_code)]
             unsafe {
-                sp1_lib::sys_bigint(
+                sp1_lib::syscall_uint256_mulmod(
                     &mut result as *mut [u64; 4],
-                    0,
-                    &lhs as *const [u64; 4],
                     &rhs_l as *const [u64; 4],
-                    &self.ml as *const [u64; 4],
                 );
             }
             return Fp::new(U256::from_le_bytes(bytemuck::cast(result)), self.m, self.ml);
