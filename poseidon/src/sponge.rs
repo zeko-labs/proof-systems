03d3db1f9357c43c50902f8e05ef96995a1ad6fa
yout <yout@msn.com>
2026-04-25 11:16:12 +0200

    update sponge

--- poseidon/src/sponge.rs
+++ poseidon/src/sponge.rs
@@ -7,6 +7,7 @@ use crate::{
 use alloc::{vec, vec::Vec};
 use ark_ec::models::short_weierstrass::{Affine, SWCurveConfig};
 use ark_ff::{BigInteger, Field, One, PrimeField, Zero};
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 
 /// Abstracts a sponge operating on a base field `Fq` of the curve
 /// `G`. The parameter `Fr` models the scalar field of the curve.
@@ -38,7 +39,6 @@ pub trait FqSponge<Fq: Field, G, Fr, const FULL_ROUNDS: usize> {
 }
 
 pub const CHALLENGE_LENGTH_IN_LIMBS: usize = 2;
-
 const HIGH_ENTROPY_LIMBS: usize = 2;
 
 /// A challenge used as a scalar on a group element in the verifier.
@@ -50,6 +50,7 @@ pub fn endo_coefficient<F: PrimeField>() -> F {
     F::GENERATOR.pow(p_minus_1_over_3.into_bigint().as_ref())
 }
 
+#[inline(always)]
 fn get_bit(limbs_lsb: &[u64], i: u64) -> u64 {
     let limb = i / 64;
     let j = i % 64;
@@ -91,12 +92,16 @@ impl<F: PrimeField> ScalarChallenge<F> {
 }
 
 #[derive(Clone)]
-pub struct DefaultFqSponge<P: SWCurveConfig, SC: SpongeConstants, const FULL_ROUNDS: usize> {
+pub struct DefaultFqSponge<P: SWCurveConfig, SC: SpongeConstants, const FULL_ROUNDS: usize>
+where
+    P::BaseField: PrimeField + CanonicalSerialize + CanonicalDeserialize,
+{
     pub sponge: ArithmeticSponge<P::BaseField, SC, FULL_ROUNDS>,
     pub last_squeezed: Vec<u64>,
 }
 
-pub struct DefaultFrSponge<Fr: Field, SC: SpongeConstants, const FULL_ROUNDS: usize> {
+pub struct DefaultFrSponge<Fr: PrimeField + CanonicalSerialize + CanonicalDeserialize, SC: SpongeConstants, const FULL_ROUNDS: usize>
+{
     pub sponge: ArithmeticSponge<Fr, SC, FULL_ROUNDS>,
     pub last_squeezed: Vec<u64>,
 }
@@ -104,7 +109,7 @@ pub struct DefaultFrSponge<Fr: Field, SC: SpongeConstants, const FULL_ROUNDS: us
 impl<const FULL_ROUNDS: usize, Fr> From<&'static ArithmeticSpongeParams<Fr, FULL_ROUNDS>>
     for DefaultFrSponge<Fr, PlonkSpongeConstantsKimchi, FULL_ROUNDS>
 where
-    Fr: PrimeField,
+    Fr: PrimeField + CanonicalSerialize + CanonicalDeserialize,
 {
     fn from(p: &'static ArithmeticSpongeParams<Fr, FULL_ROUNDS>) -> Self {
         DefaultFrSponge {
@@ -114,6 +119,7 @@ where
     }
 }
 
+#[inline(always)]
 fn pack<B: BigInteger>(limbs_lsb: &[u64]) -> B {
     let mut res: B = 0u64.into();
     for &x in limbs_lsb.iter().rev() {
@@ -123,51 +129,68 @@ fn pack<B: BigInteger>(limbs_lsb: &[u64]) -> B {
     res
 }
 
-impl<Fr: PrimeField, SC: SpongeConstants, const FULL_ROUNDS: usize>
+#[inline(always)]
+fn take_first_limbs(buf: &mut Vec<u64>, num_limbs: usize) -> Vec<u64> {
+    debug_assert!(buf.len() >= num_limbs);
+
+    let mut out = Vec::with_capacity(num_limbs);
+    out.extend_from_slice(&buf[..num_limbs]);
+
+    let remaining = buf.len() - num_limbs;
+    for i in 0..remaining {
+        buf[i] = buf[num_limbs + i];
+    }
+    buf.truncate(remaining);
+
+    out
+}
+
+impl<Fr: PrimeField + CanonicalSerialize + CanonicalDeserialize, SC: SpongeConstants, const FULL_ROUNDS: usize>
     DefaultFrSponge<Fr, SC, FULL_ROUNDS>
 {
-    pub fn squeeze(&mut self, num_limbs: usize) -> Fr {
-        if self.last_squeezed.len() >= num_limbs {
-            let last_squeezed = self.last_squeezed.clone();
-            let (limbs, remaining) = last_squeezed.split_at(num_limbs);
-            self.last_squeezed = remaining.to_vec();
-            Fr::from(pack::<Fr::BigInt>(limbs))
-        } else {
-            let x = self.sponge.squeeze().into_bigint();
+    #[inline(always)]
+    fn refill_limbs(&mut self) {
+        let x = self.sponge.squeeze().into_bigint();
+        self.last_squeezed
+            .extend_from_slice(&x.as_ref()[0..HIGH_ENTROPY_LIMBS]);
+    }
 
-            self.last_squeezed
-                .extend(&x.as_ref()[0..HIGH_ENTROPY_LIMBS]);
-            self.squeeze(num_limbs)
+    pub fn squeeze(&mut self, num_limbs: usize) -> Fr {
+        while self.last_squeezed.len() < num_limbs {
+            self.refill_limbs();
         }
+
+        Fr::from(pack::<Fr::BigInt>(&take_first_limbs(
+            &mut self.last_squeezed,
+            num_limbs,
+        )))
     }
 }
 
 impl<P: SWCurveConfig, SC: SpongeConstants, const FULL_ROUNDS: usize>
     DefaultFqSponge<P, SC, FULL_ROUNDS>
 where
-    P::BaseField: PrimeField,
+    P::BaseField: PrimeField + CanonicalSerialize + CanonicalDeserialize,
     <P::BaseField as PrimeField>::BigInt: Into<<P::ScalarField as PrimeField>::BigInt>,
 {
-    pub fn squeeze_limbs(&mut self, num_limbs: usize) -> Vec<u64> {
-        if self.last_squeezed.len() >= num_limbs {
-            let last_squeezed = self.last_squeezed.clone();
-            let (limbs, remaining) = last_squeezed.split_at(num_limbs);
-            self.last_squeezed = remaining.to_vec();
-            limbs.to_vec()
-        } else {
-            let x = self.sponge.squeeze().into_bigint();
+    #[inline(always)]
+    fn refill_limbs(&mut self) {
+        let x = self.sponge.squeeze().into_bigint();
+        self.last_squeezed
+            .extend_from_slice(&x.as_ref()[0..HIGH_ENTROPY_LIMBS]);
+    }
 
-            self.last_squeezed
-                .extend(&x.as_ref()[0..HIGH_ENTROPY_LIMBS]);
-            self.squeeze_limbs(num_limbs)
+    pub fn squeeze_limbs(&mut self, num_limbs: usize) -> Vec<u64> {
+        while self.last_squeezed.len() < num_limbs {
+            self.refill_limbs();
         }
+
+        take_first_limbs(&mut self.last_squeezed, num_limbs)
     }
 
     pub fn squeeze_field(&mut self) -> P::BaseField {
-        self.last_squeezed = vec![];
-        let out = self.sponge.squeeze();
-
-        out
+        self.last_squeezed.clear();
+        self.sponge.squeeze()
     }
 
     pub fn squeeze(&mut self, num_limbs: usize) -> P::ScalarField {
@@ -180,7 +203,7 @@ impl<P: SWCurveConfig, SC: SpongeConstants, const FULL_ROUNDS: usize>
     FqSponge<P::BaseField, Affine<P>, P::ScalarField, FULL_ROUNDS>
     for DefaultFqSponge<P, SC, FULL_ROUNDS>
 where
-    P::BaseField: PrimeField,
+    P::BaseField: PrimeField + CanonicalSerialize + CanonicalDeserialize,
     <P::BaseField as PrimeField>::BigInt: Into<<P::ScalarField as PrimeField>::BigInt>,
 {
     fn new(params: &'static ArithmeticSpongeParams<P::BaseField, FULL_ROUNDS>) -> Self {
@@ -192,43 +215,53 @@ where
     }
 
     fn absorb_g(&mut self, g: &[Affine<P>]) {
-        self.last_squeezed = vec![];
-        for g in g.iter() {
-            if g.infinity {
+        self.last_squeezed.clear();
+
+        let mut buf = Vec::with_capacity(2 * g.len());
+        for point in g.iter() {
+            if point.infinity {
                 // Absorb a fake point (0, 0).
                 let zero = P::BaseField::zero();
-                self.sponge.absorb(&[zero]);
-                self.sponge.absorb(&[zero]);
+                buf.push(zero);
+                buf.push(zero);
             } else {
-                self.sponge.absorb(&[g.x]);
-                self.sponge.absorb(&[g.y]);
+                buf.push(point.x);
+                buf.push(point.y);
             }
         }
+
+        self.sponge.absorb(&buf);
     }
 
     fn absorb_fq(&mut self, x: &[P::BaseField]) {
-        self.last_squeezed = vec![];
-
-        for fe in x {
-            self.sponge.absorb(&[*fe]);
-        }
+        self.last_squeezed.clear();
+        self.sponge.absorb(x);
     }
 
     fn absorb_fr(&mut self, x: &[P::ScalarField]) {
-        self.last_squeezed = vec![];
+        self.last_squeezed.clear();
 
-        x.iter().for_each(|x| {
-            let bits = x.into_bigint().to_bits_le();
+        if <P::ScalarField as PrimeField>::MODULUS
+            < <P::BaseField as PrimeField>::MODULUS.into()
+        {
+            let mut buf = Vec::with_capacity(x.len());
 
-            if <P::ScalarField as PrimeField>::MODULUS
-                < <P::BaseField as PrimeField>::MODULUS.into()
-            {
+            for scalar in x.iter() {
+                let bits = scalar.into_bigint().to_bits_le();
                 let fe = P::BaseField::from_bigint(
                     <P::BaseField as PrimeField>::BigInt::from_bits_le(&bits),
                 )
                 .expect("padding code has a bug");
-                self.sponge.absorb(&[fe]);
-            } else {
+                buf.push(fe);
+            }
+
+            self.sponge.absorb(&buf);
+        } else {
+            let mut buf = Vec::with_capacity(2 * x.len());
+
+            for scalar in x.iter() {
+                let bits = scalar.into_bigint().to_bits_le();
+
                 let low_bit = if bits[0] {
                     P::BaseField::one()
                 } else {
@@ -240,18 +273,19 @@ where
                 )
                 .expect("padding code has a bug");
 
-                self.sponge.absorb(&[high_bits]);
-                self.sponge.absorb(&[low_bit]);
+                buf.push(high_bits);
+                buf.push(low_bit);
             }
-        });
+
+            self.sponge.absorb(&buf);
+        }
     }
 
     fn digest(mut self) -> P::ScalarField {
         let x: <P::BaseField as PrimeField>::BigInt = self.squeeze_field().into_bigint();
+
         // Returns zero for values that are too large.
-        // This means that there is a bias for the value zero (in one of the curve).
-        // An attacker could try to target that seed, in order to predict the challenges
-        // produced by the Fr sponge.
+        // This means that there is a bias for the value zero in one of the curves.
         // Since log2(q - p) is much smaller than log2(q), the attack remains negligible.
         P::ScalarField::from_bigint(x.into()).unwrap_or_else(P::ScalarField::zero)
     }
@@ -268,10 +302,6 @@ where
         self.squeeze_field()
     }
 }
-
-//
-// OCaml types
-//
 
 #[cfg(feature = "ocaml_types")]
 #[allow(non_local_definitions)]
